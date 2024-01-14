55ac5a048870ada52b369393421a1646ef0063f2
Arda Demirel <yademirel21@gmail.com>
2024-01-14 18:42:08 +0300

    Added port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.get('/api/time', (req, res) => {
     const date = new Date();
     res.send(date.toGMTString());
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}...`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}...`);
 });
